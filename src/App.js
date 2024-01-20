3d5c4cfd4ec03846ef3652cc554a622d4d656617
samggg <gomessamuel.14@gmail.com>
2024-01-19 21:16:38 -0400

    2º atualização

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { IoMdSearch } from 'react-icons/io';
 
 function App() {
+  // Estado para armazenar o nome da cidade
+  const [cidade, setCidade] = useState("");
+  // Estado para armazenar os dados da previsão do tempo
+  const [previsao, setPrevisao] = useState(null);
 
-  const [cidade, setCidade] = useState("")
-  const [previsao, setPrevisao] = useState(null)
+  // URL da API com a chave e o idioma configurados
+  const url = `https://api.weatherapi.com/v1/current.json?key=49f1834cf8264440ad122012240601&q=${cidade}&lang=pt`;
 
+  // Função para lidar com a mudança no input
   const handleChange = (event) => {
-   setCidade(event.target.value)
-  }
-  const baseURL = `http://api.weatherapi.com/v1/current.json?key=49f1834cf8264440ad122012240601&q=${cidade}&lang=pt`
-  const handleSearch = () => {
-    fetch(baseURL
-      )
-        .then((response) => {
-          if (response.status === 200) {
-            return response.json();
-          }
-        })
-        .then((data) => {
-          setPrevisao(data);
-        });
-    };
+    setCidade(event.target.value);
+  };
+
+  // Função para lidar com a pesquisa do tempo
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        setPrevisao(data);
+      } else {
+        console.error('Erro na chamada à API:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Erro na chamada à API:', error);
+    }
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
@@ -37,39 +45,38 @@ function App() {
 
           <div className="row mb-4">
             <div className="col-md-12">
-              <input 
+              {/* Input para inserir o nome da cidade */}
+              <input
                 onChange={handleChange}
-                className="form-control" 
+                className="form-control"
                 value={cidade}
                 placeholder="Digite o nome da cidade"
               />
             </div>
           </div>
 
-            <button onClick={handleSearch} className="btn btn-primary btn-lg">
-              <IoMdSearch className="mr-2" /> Pesquisar
-            </button>
+          {/* Botão de pesquisa */}
+          <button onClick={handleSearch} className="btn btn-primary btn-lg">
+            <IoMdSearch className="mr-2" /> Pesquisar
+          </button>
 
-
-            {previsao ? (
-              <div className="mt-5 d-flex flex-column align-items-center">
-                {previsao.current.condition.icon && (
-                  <div>
-                    <img src={previsao.current.condition.icon} alt="Condição do Tempo" />
-                  </div>
-                )}
-
-                <div className="mt-3">
-                  <h3> Hoje o dia está: {previsao.current.condition.text}</h3>
-                  <p className='lead'>
-                    Temperatura: {previsao.current.temp_c} °C
-                  </p>
+          {/* Exibição da previsão do tempo, se disponível */}
+          {previsao ? (
+            <div className="mt-5 d-flex flex-column align-items-center">
+              {previsao.current.condition.icon && (
+                <div>
+                  <img src={previsao.current.condition.icon} alt="Condição do Tempo" />
                 </div>
+              )}
+
+              <div className="mt-3">
+                <h3> Hoje o dia está: {previsao.current.condition.text}</h3>
+                <p className='lead'>
+                  Temperatura: {previsao.current.temp_c} °C
+                </p>
               </div>
-            ) : null}
-
-
-
+            </div>
+          ) : null}
         </div>
       </main>
     </div>
